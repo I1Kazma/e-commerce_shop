145c15299d773ac10871bd65a6c4e1bde8053832
I1K <karmakazmenko@gmail.com>
2025-07-20 15:32:17 +0200

    added HomePage layout

---
+++ src/components/CategoryList.tsx
@@ -0,0 +1,90 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const CategoryList = async () => {
+  const cats = [
+    {
+      _id: 1,
+      name: "Summer Sale Collections",
+      description: "Sale! Up to 50% off!",
+      media:
+        "https://i.pinimg.com/736x/dc/46/8b/dc468be47bc0cf1324c092f776383709.jpg",
+      slug: "/",
+      bg: "bg-gradient-to-r from-yellow-50 to-pink-50",
+    },
+    {
+      _id: 2,
+      name: "Winter Sale Collections",
+      description: "Sale! Up to 50% off!",
+      media:
+        "https://i.pinimg.com/736x/c1/f2/1d/c1f21dd2419929fe033ebf550773d148.jpg",
+      slug: "/",
+      bg: "bg-gradient-to-r from-pink-50 to-blue-50",
+    },
+    {
+      _id: 3,
+      name: "Spring Sale Collections",
+      description: "Sale! Up to 50% off!",
+      media:
+        "https://i.pinimg.com/736x/5d/29/e5/5d29e5fb77e68fc4731ae45e49babecd.jpg",
+      slug: "/",
+      bg: "bg-gradient-to-r from-blue-50 to-yellow-50",
+    },
+    {
+      _id: 4,
+      name: "Spring Sale Collections",
+      description: "Sale! Up to 50% off!",
+      media:
+        "https://i.pinimg.com/736x/5d/29/e5/5d29e5fb77e68fc4731ae45e49babecd.jpg",
+      slug: "/",
+      bg: "bg-gradient-to-r from-blue-50 to-yellow-50",
+    },
+    {
+      _id: 5,
+      name: "Spring Sale Collections",
+      description: "Sale! Up to 50% off!",
+      media:
+        "https://i.pinimg.com/736x/5d/29/e5/5d29e5fb77e68fc4731ae45e49babecd.jpg",
+      slug: "/",
+      bg: "bg-gradient-to-r from-blue-50 to-yellow-50",
+    },
+    {
+      _id: 6,
+      name: "Spring Sale Collections",
+      description: "Sale! Up to 50% off!",
+      media:
+        "https://i.pinimg.com/736x/5d/29/e5/5d29e5fb77e68fc4731ae45e49babecd.jpg",
+      slug: "/",
+      bg: "bg-gradient-to-r from-blue-50 to-yellow-50",
+    },
+  ];
+
+  return (
+    <div className="px-4 overflow-x-scroll scrollbar-hide">
+      <div className="flex gap-4 md:gap-8">
+        {cats.map((item) => (
+          <Link
+            href={`/list?cat=${item.slug}`}
+            className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+            key={item._id}
+          >
+            <div className="relative bg-slate-100 w-full h-96">
+              <Image
+                src={item.media || "cat.png"}
+                alt=""
+                fill
+                sizes="20vw"
+                className="object-cover"
+              />
+            </div>
+            <h1 className="mt-8 font-light text-xl tracking-wide">
+              {item.name}
+            </h1>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CategoryList;
---
+++ src/components/ProductList.tsx
@@ -0,0 +1,137 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+const ProductList = () => {
+  return (
+    <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://i.pinimg.com/736x/dc/46/8b/dc468be47bc0cf1324c092f776383709.jpg"
+            fill
+            sizes="25vw"
+            alt=""
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://i.pinimg.com/736x/c1/f2/1d/c1f21dd2419929fe033ebf550773d148.jpg"
+            fill
+            sizes="25vw"
+            alt=""
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+        </div>
+        <button className="rounded-2xl ring-1 ring-main text-main w-max py-2 px-4 text-xs hover:bg-main hover:text-white transition ease-in-out">
+          Add to Cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://i.pinimg.com/736x/dc/46/8b/dc468be47bc0cf1324c092f776383709.jpg"
+            fill
+            sizes="25vw"
+            alt=""
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://i.pinimg.com/736x/c1/f2/1d/c1f21dd2419929fe033ebf550773d148.jpg"
+            fill
+            sizes="25vw"
+            alt=""
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        </div>
+        <button className="rounded-2xl ring-1 ring-main text-main w-max py-2 px-4 text-xs hover:bg-main hover:text-white transition ease-in-out">
+          Add to Cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://i.pinimg.com/736x/dc/46/8b/dc468be47bc0cf1324c092f776383709.jpg"
+            fill
+            sizes="25vw"
+            alt=""
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://i.pinimg.com/736x/c1/f2/1d/c1f21dd2419929fe033ebf550773d148.jpg"
+            fill
+            sizes="25vw"
+            alt=""
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+        </div>
+        <button className="rounded-2xl ring-1 ring-main text-main w-max py-2 px-4 text-xs hover:bg-main hover:text-white transition ease-in-out">
+          Add to Cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://i.pinimg.com/736x/dc/46/8b/dc468be47bc0cf1324c092f776383709.jpg"
+            fill
+            sizes="25vw"
+            alt=""
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://i.pinimg.com/736x/c1/f2/1d/c1f21dd2419929fe033ebf550773d148.jpg"
+            fill
+            sizes="25vw"
+            alt=""
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+        </div>
+        <button className="rounded-2xl ring-1 ring-main text-main w-max py-2 px-4 text-xs hover:bg-main hover:text-white transition ease-in-out">
+          Add to Cart
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default ProductList;
